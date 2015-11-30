1ddec2af5d380f8007aa359d2427fae06e40c283
zl <simple0812@sina.cn>
2015-11-30 10:17:12 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+#!node
+
+var program = require('commander');
+var fse = require('fs-extra');
+
+function range(val) {
+    return val.split('..').map(Number);
+}
+
+function list(val) {
+    return val.split(',');
+}
+
+function collect(val, memo) {
+    memo.push(val);
+    return memo;
+}
+
+function increaseVerbosity(v, total) {
+    return total + 1;
+}
+
+program
+    .version('0.0.1')
+    .option('-e, --express', 'express')
+    .option('-k, --koa', 'koa')
+    .parse(process.argv);
+
+
+if (program.express) {
+    console.log(process.PWD, __dirname)
+    var currDir = process.cwd();
+
+    fse.copy(__dirname + '/frames/express', currDir, function(err) {
+        if(err) return console.error(err)
+        console.log('ok')
+    })
+}
+
+if (program.koa) {
+    var currDir = process.cwd();
+}
